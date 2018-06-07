20efd156465e6484bebc87eb38e8eb0c68d4738c
Venkat <Venkatgopi@hotmail.com>
2018-06-07 19:26:51 +0200

    Hotel Management

---
+++ src/main/java/com/example/demo/controller/WebRestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+	@RequestMapping("/api/hi")
+	public String hi() {
+		return "Hello World from Restful API";
+	}
+	
+	@RequestMapping("/api/booking")
+	public String booking() {
+		return "Booking logic trigger here";
+	}
+}
